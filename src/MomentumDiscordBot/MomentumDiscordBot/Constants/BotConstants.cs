164c73673eee67014c345a78e7f51522ada24833
Luke Parker <lukemparker@outlook.com>
2020-03-19 16:20:39 +1000

    Create logging service
    Also hooks to DiscordSocketClient log event

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Constants/BotConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MomentumDiscordBot.Constants
+{
+    public static class BotConstants
+    {
+        internal const int LogPaddingLength = 10;
+    }
+}
---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Constants/ColorConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MomentumDiscordBot.Constants
+{
+    public static class ColorConstants
+    {
+        public static readonly ConsoleColor InfoLogColor = ConsoleColor.Cyan;
+        public static readonly ConsoleColor WarningLogColor = ConsoleColor.DarkYellow;
+        public static readonly ConsoleColor ErrorLogColor = ConsoleColor.Red;
+    }
+}
