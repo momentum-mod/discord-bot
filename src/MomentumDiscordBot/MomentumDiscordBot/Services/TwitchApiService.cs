a0f2f679bb1cdc49f9f283557e87863baee845fc
Luke Parker <lukemparker@outlook.com>
2020-03-19 17:29:01 +1000

    Create TwitchApiService, with a function to get current mmod streamers

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/TwitchApiService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MomentumDiscordBot.Constants;
+using TwitchLib.Api;
+using Stream = TwitchLib.Api.Helix.Models.Streams.Stream;
+
+namespace MomentumDiscordBot.Services
+{
+    public class TwitchApiService
+    {
+        private readonly TwitchAPI _apiService;
+        public TwitchApiService()
+        {
+            if (File.Exists(PathConstants.DiscordTokenFilePath))
+            {
+                // File exists, get the text
+                var twitchClientId = File.ReadAllText(PathConstants.TwitchAPIClientIdFilePath);
+                _apiService = new TwitchAPI();
+                _apiService.Settings.ClientId = twitchClientId;
+            }
+            else
+            {
+                throw new Exception($"No Twitch API token file exists, expected it at: '{PathConstants.TwitchAPIClientIdFilePath}'");
+            }
+        }
+
+        public async Task<List<Stream>> GetLiveMomentumModStreamersAsync()
+        {
+            var streams = await _apiService.Helix.Streams.GetStreamsAsync(gameIds: new List<string>
+                {BotConstants.MomentumModTwitchID.ToString()});
+            return streams.Streams.ToList();
+        }
+    }
+}
