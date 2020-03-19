160bbabc1a0435e55bf867c414a682c87cc4f601
Luke Parker <lukemparker@outlook.com>
2020-03-19 17:36:12 +1000

    Create Config model

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MomentumDiscordBot.Models
+{
+    public class Config
+    {
+        [JsonProperty("streamer_channel")] public ulong MomentumModStreamerChannelId { get; set; }
+    }
+}
