e1229429ad63590610a04cc7fed018fa4ad77480
Luke Parker <lukemparker@outlook.com>
2020-03-19 18:23:01 +1000

    Create Dockerfile, and helper install script

---
+++ runDocker.sh
@@ -0,0 +1,11 @@
+echo '-= Stopping momentum-discord Production Container =-'
+docker container stop momentum-discord-production
+
+echo '-= Removing Old momentum-discord Production Container =-'
+docker container rm momentum-discord-production
+
+echo '-= Building Docker Image from Dockerfile =-\n'
+docker build -t momentum-discord ./src/MomentumDiscordBot/MomentumDiscordBot/
+
+echo '-= Runnning the Image =-\n'
+docker run --restart on-failure:5 --name "momentum-discord-production" -d momentum-discord
