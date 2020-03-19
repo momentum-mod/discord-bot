e53e614ada9e1795360163f1f4877e675c8922c9
Luke Parker <lukemparker@outlook.com>
2020-03-19 17:28:29 +1000

    Add Twitch Client ID and config to PathConstants

--- src/MomentumDiscordBot/MomentumDiscordBot/Constants/PathConstants.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Constants/PathConstants.cs
@@ -7,5 +7,7 @@ namespace MomentumDiscordBot.Constants
     {
         internal static readonly string ConfigFolderPath = Path.Combine(Environment.CurrentDirectory, "config");
         internal static readonly string DiscordTokenFilePath = Path.Combine(ConfigFolderPath, "discordToken.txt");
+        internal static readonly string TwitchAPIClientIdFilePath = Path.Combine(ConfigFolderPath, "twitchApiClientId.txt");
+        internal static readonly string ConfigFilePath = Path.Combine(ConfigFolderPath, "config.json");
     }
 }
