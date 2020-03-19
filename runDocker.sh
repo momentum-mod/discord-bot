eb6497cf3549ba23cf854c54cf8bb2f341f40071
Luke Parker <lukemparker@outlook.com>
2020-03-19 18:29:15 +1000

    Expose ports to docker container

--- runDocker.sh
+++ runDocker.sh
@@ -8,4 +8,4 @@ echo '-= Building Docker Image from Dockerfile =-\n'
 docker build -t momentum-discord ./src/MomentumDiscordBot/MomentumDiscordBot/
 
 echo '-= Runnning the Image =-\n'
-docker run --restart on-failure:5 --name "momentum-discord-production" -d momentum-discord
+docker run --network host --restart on-failure:5 --name "momentum-discord-production" -d momentum-discord
