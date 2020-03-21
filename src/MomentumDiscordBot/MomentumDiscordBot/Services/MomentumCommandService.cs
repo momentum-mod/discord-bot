9f97e16c1b08052d62224ccd22a5052b875250db
Luke Parker <lukemparker@outlook.com>
2020-03-21 10:15:24 +1000

    Create static command to build Discord.NET base command service

--- src/MomentumDiscordBot/MomentumDiscordBot/Services/MomentumCommandService.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/MomentumCommandService.cs
@@ -1,15 +1,22 @@
 ﻿using Discord;
 using Discord.Commands;
-using Discord.WebSocket;
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace MomentumDiscordBot.Services
 {
     public class MomentumCommandService
     {
+        public static CommandService BuildBaseCommandService()
+        {
+            var commandServiceConfig = new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false,
+                // Default RunMode to async to prevent commands from blocking the thread
+                DefaultRunMode = RunMode.Async,
+                IgnoreExtraArgs = true,
+                LogLevel = LogSeverity.Info
+            };
 
+            return new CommandService(commandServiceConfig);
+        }
     }
 }
