ef9dccd3f2346c1e03d885ade82e5ace8d9fb131
Luke Parker <lukemparker@outlook.com>
2020-03-21 10:19:24 +1000

    Create constructor with needed classes

--- src/MomentumDiscordBot/MomentumDiscordBot/Services/MomentumCommandService.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/MomentumCommandService.cs
@@ -1,10 +1,24 @@
-﻿using Discord;
+﻿using System;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace MomentumDiscordBot.Services
 {
     public class MomentumCommandService
     {
+        private readonly DiscordSocketClient _discordClient;
+        private readonly CommandService _baseCommandService;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly LogService _logService;
+        public MomentumCommandService(DiscordSocketClient discordClient, CommandService baseCommandService, IServiceProvider serviceProvider, LogService logService)
+        {
+            // Parameters are injected
+            _discordClient = discordClient;
+            _baseCommandService = baseCommandService;
+            _serviceProvider = serviceProvider;
+            _logService = logService;
+        }
         public static CommandService BuildBaseCommandService()
         {
             var commandServiceConfig = new CommandServiceConfig
