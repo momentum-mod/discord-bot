bc93016b13da28a27955fe2adf86f7c144921ef1
Luke Parker <lukemparker@outlook.com>
2020-03-21 10:21:44 +1000

    Add command prefix to config.json

--- src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
@@ -5,5 +5,6 @@ namespace MomentumDiscordBot.Models
     public class Config
     {
         [JsonProperty("streamer_channel")] public ulong MomentumModStreamerChannelId { get; set; }
+        [JsonProperty("command_prefix")] public string CommandPrefix { get; set; }
     }
 }
