7350f954289b7b79351b679ac651edcb40dd2e59
Luke Parker <lukemparker@outlook.com>
2020-03-21 10:14:49 +1000

    Create MomentumCommandService.cs

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/MomentumCommandService.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MomentumDiscordBot.Services
+{
+    public class MomentumCommandService
+    {
+
+    }
+}
