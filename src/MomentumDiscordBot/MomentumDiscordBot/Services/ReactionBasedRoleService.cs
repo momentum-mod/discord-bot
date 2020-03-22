1601f8bd39617ce6f92c69603d93159f0f708acf
Luke Parker <lukemparker@outlook.com>
2020-03-22 11:48:24 +1000

    Create constructor for role service

--- src/MomentumDiscordBot/MomentumDiscordBot/Services/ReactionBasedRoleService.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/ReactionBasedRoleService.cs
@@ -1,6 +1,16 @@
-﻿namespace MomentumDiscordBot.Services
+﻿using System.Collections.Generic;
+using Discord.WebSocket;
+
+namespace MomentumDiscordBot.Services
 {
     public class ReactionBasedRoleService
     {
+        private readonly DiscordSocketClient _discordClient;
+        private readonly SocketTextChannel _textChannel;
+        public ReactionBasedRoleService(DiscordSocketClient discordClient, ulong channelId)
+        {
+            _discordClient = discordClient;
+            _textChannel = _discordClient.GetChannel(channelId) as SocketTextChannel;
+        }
     }
 }
