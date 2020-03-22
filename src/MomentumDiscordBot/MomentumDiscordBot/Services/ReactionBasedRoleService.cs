0b60665e336442fd04d38918b6f3957b6a22cb56
Luke Parker <lukemparker@outlook.com>
2020-03-22 12:36:01 +1000

    Add and remove the role when a reaction is added/removed

--- src/MomentumDiscordBot/MomentumDiscordBot/Services/ReactionBasedRoleService.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/ReactionBasedRoleService.cs
@@ -36,25 +36,62 @@ namespace MomentumDiscordBot.Services
 
             foreach (var message in existingMessages)
             {
-                if (message.Embeds.Count == 1)
+                if (TryParseRoleFromEmbed(message, out var role))
                 {
-                    var embed = message.Embeds.First();
-                    if (MentionUtils.TryParseRole(embed.Title, out var roleId))
-                    {
-                        _existingRoleEmbeds.Add(roleId, message.Id);
-                    }
+                    _existingRoleEmbeds.Add(role.Id, message.Id);
                 }
             }
         }
 
+        /// <summary>
+        /// The role should be @Mentioned in the Title of the Embed
+        /// </summary>
+        private bool TryParseRoleFromEmbed(IMessage input, out IRole role)
+        {
+            if (input.Embeds.Count == 1)
+            {
+                var embed = input.Embeds.First();
+                if (MentionUtils.TryParseRole(embed.Title, out var roleId))
+                {
+                    // We have the role ID, want the IRole - search guilds for the correct channel
+                    var guild = _discordClient.Guilds.First(x => x.Channels.Any(y => y.Id == _textChannel.Id));
+                    role = guild.GetRole(roleId);
+                    return true;
+                }
+            }
+
+            role = null;
+            return false;
+        }
+
         private async Task ReactionAdded(Cacheable<IUserMessage, ulong> messageBefore, ISocketMessageChannel messageAfter, SocketReaction reaction)
         {
-            
+            if (reaction.Channel.Id != _textChannel.Id) return;
+
+            // Check that the message reacted to is a role embed
+            if (_existingRoleEmbeds.ContainsKey(reaction.MessageId))
+            {
+                var user = _discordClient.GetUser(reaction.UserId);
+                if (user is SocketGuildUser guildUser && TryParseRoleFromEmbed(messageBefore.Value, out var role))
+                { 
+                    await guildUser.AddRoleAsync(role);
+                }
+            }
         }
 
         private async Task ReactionRemoved(Cacheable<IUserMessage, ulong> messageBefore, ISocketMessageChannel messageAfter, SocketReaction reaction)
         {
+            if (reaction.Channel.Id != _textChannel.Id) return;
 
+            // Check that the message reacted to is a role embed
+            if (_existingRoleEmbeds.ContainsKey(reaction.MessageId))
+            {
+                var user = _discordClient.GetUser(reaction.UserId);
+                if (user is SocketGuildUser guildUser && TryParseRoleFromEmbed(messageBefore.Value, out var role))
+                {
+                    await guildUser.RemoveRoleAsync(role);
+                }
+            }
         }
     }
 }
