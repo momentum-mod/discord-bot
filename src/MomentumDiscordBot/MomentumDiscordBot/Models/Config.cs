64b3cdba601cf74aa59c5fdc70198d9c7750c713
Luke Parker <lukemparker@outlook.com>
2020-03-22 11:56:31 +1000

    Add roles channel to config.json

--- src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
@@ -5,6 +5,7 @@ namespace MomentumDiscordBot.Models
     public class Config
     {
         [JsonProperty("streamer_channel")] public ulong MomentumModStreamerChannelId { get; set; }
+        [JsonProperty("roles_channel")] public ulong RolesChannelId { get; set; }
         [JsonProperty("command_prefix")] public string CommandPrefix { get; set; }
     }
 }
