5b4f66e3b1311f73cfae391a8b95142948aa731e
Luke Parker <lukemparker@outlook.com>
2020-03-22 13:03:36 +1000

    Add moderator role and admin role to config, make the JsonAttribute the private members so that the file isn't saved on loading

--- src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
@@ -7,14 +7,15 @@ namespace MomentumDiscordBot.Models
 {
     public class Config
     {
-        private ulong _momentumModStreamerChannelId;
-        private ulong _rolesChannelId;
-        private string _commandPrefix;
-        private IRole[] _mentionRoles;
 
-        [JsonConstructor]
+        [JsonProperty("streamer_channel")] private ulong _momentumModStreamerChannelId;
+        [JsonProperty("roles_channel")] private ulong _rolesChannelId;
+        [JsonProperty("command_prefix")] private string _commandPrefix;
+        [JsonProperty("mention_roles")] private IRole[] _mentionRoles;
+        [JsonProperty("moderator_id")] private ulong _moderatorRoleID;
+        [JsonProperty("admin_id")] private ulong _adminRoleID;
+
         public Config() { }
-
         public static Config LoadFromFile()
         {
             if (File.Exists(PathConstants.ConfigFilePath))
@@ -28,38 +29,40 @@ namespace MomentumDiscordBot.Models
                 throw new FileNotFoundException($"No config file exists, expected it at: '{PathConstants.ConfigFilePath}'");
             }
         }
-
         private void SaveToFile()
         {
             var configString = JsonConvert.SerializeObject(this);
             File.WriteAllText(PathConstants.ConfigFilePath, configString);
         }
-
-        [JsonProperty("streamer_channel")]
         public ulong MomentumModStreamerChannelId
         {
             get => _momentumModStreamerChannelId;
             set { _momentumModStreamerChannelId = value; SaveToFile(); }
         }
-
-        [JsonProperty("roles_channel")]
         public ulong RolesChannelId
         {
             get => _rolesChannelId;
             set { _rolesChannelId = value; SaveToFile(); }
         }
-
-        [JsonProperty("command_prefix")]
         public string CommandPrefix
         {
             get => _commandPrefix;
             set { _commandPrefix = value; SaveToFile(); }
         }
-
         public IRole[] MentionRoles
         {
             get => _mentionRoles;
             set { _mentionRoles = value; SaveToFile(); }
+        }
+        public ulong ModeratorRoleID
+        {
+            get => _moderatorRoleID;
+            set { _moderatorRoleID = value; SaveToFile(); }
+        }
+        public ulong AdminRoleID
+        {
+            get => _adminRoleID;
+            set { _adminRoleID = value; SaveToFile(); }
         }
     }
 }
