150b517bfba5a5e7e972473807ef1abea9985071
Luke Parker <lukemparker@outlook.com>
2020-03-22 19:53:59 +1000

    Refactor preconditions requiring a role from ID, to a utility method

--- src/MomentumDiscordBot/MomentumDiscordBot/Discord/Precondition/ModeratorPreconditionAttribute.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Precondition/ModeratorPreconditionAttribute.cs
@@ -5,6 +5,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using MomentumDiscordBot.Models;
+using MomentumDiscordBot.Utilities;
 
 namespace MomentumDiscordBot.Discord.Precondition
 {
@@ -13,16 +14,8 @@ namespace MomentumDiscordBot.Discord.Precondition
         public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command,
             IServiceProvider services)
         {
-            // Check if this user is a Guild User, which is the only context where roles exist
-            if (context.User is SocketGuildUser user)
-            {
-                var config = services.GetRequiredService<Config>();
-                if (user.Roles.Any(x => x.Id == config.ModeratorRoleID))
-                    return Task.FromResult(PreconditionResult.FromSuccess());
-                return Task.FromResult(PreconditionResult.FromError("You must be a Moderator to use that command"));
-            }
-
-            return Task.FromResult(PreconditionResult.FromError("You must be a user"));
+            var config = services.GetRequiredService<Config>();
+            return context.User.RequireRoleAsync(config.ModeratorRoleID);
         }
     }
 }
---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Utilities/PreconditionUtility.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace MomentumDiscordBot.Utilities
+{
+    public static class PreconditionUtility
+    {
+        public static Task<PreconditionResult> RequireRoleAsync(this IUser user, ulong roleId)
+        {
+            // Check if this user is a Guild User, which is the only context where roles exist
+            if (!(user is SocketGuildUser guildUser))
+                return Task.FromResult(PreconditionResult.FromError("You must be a guild user"));
+
+            return Task.FromResult(guildUser.Roles.Any(x => x.Id == roleId)
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("You must be an Admin to use that command"));
+        }
+    }
+}
