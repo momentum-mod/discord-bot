54a41ef720d1eb2e5b431cd2eed9aba742875a77
Luke Parker <lukemparker@outlook.com>
2020-03-23 13:23:20 +1000

    Create RequireAdminBotChannelPreconditionAttribute.cs

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Precondition/RequireAdminBotChannelPreconditionAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using MomentumDiscordBot.Models;
+using MomentumDiscordBot.Utilities;
+
+namespace MomentumDiscordBot.Discord.Precondition
+{
+    public class RequireAdminBotChannelPreconditionAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command,
+            IServiceProvider services)
+        {
+            var config = services.GetRequiredService<Config>();
+            if (context.Channel.Id == config.AdminBotChannel)
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+
+            return Task.FromResult(PreconditionResult.FromError("You are not using the bot in the admin channel"));
+        }
+    }
+}
