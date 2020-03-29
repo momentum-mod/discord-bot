c09fe4dd353d1aa819d4558cf450bd094696e82c
Luke Parker <lukemparker@outlook.com>
2020-03-29 22:08:01 +1000

    Make more maintainble, cleanup and fix logic issues in the help command utility

--- src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/GeneralModule.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/GeneralModule.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using MomentumDiscordBot.Models;
 using MomentumDiscordBot.Utilities;
 
 namespace MomentumDiscordBot.Discord.Commands
@@ -13,6 +14,7 @@ namespace MomentumDiscordBot.Discord.Commands
     {
         public CommandService CommandService { get; set; }
         public IServiceProvider Services { get; set; }
+        public Config Config { get; set; }
         [Command("help")]
         [Summary("The command you are running")]
         public async Task HelpAsync()
@@ -20,7 +22,7 @@ namespace MomentumDiscordBot.Discord.Commands
             var message = await ReplyNewEmbedAsync("Building the help command... This message will be deleted when all help messages are sent", Color.Blue);
             foreach (var module in CommandService.Modules.Where(x => !x.Name.Contains("ModuleBase")))
             {
-                var moduleHelpEmbed = HelpCommandUtilities.GetModuleHelpEmbed(module, Context, Services);
+                var moduleHelpEmbed = HelpCommandUtilities.GetModuleHelpEmbed(module, Context, Services, Config);
                 if (moduleHelpEmbed.Fields.Length > 0)
                 {
                     await ReplyAsync(embed: moduleHelpEmbed);
--- src/MomentumDiscordBot/MomentumDiscordBot/Utilities/HelpCommandUtilities.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Utilities/HelpCommandUtilities.cs
@@ -1,33 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Discord;
 using Discord.Commands;
+using MomentumDiscordBot.Models;
 
 namespace MomentumDiscordBot.Utilities
 {
     public static class HelpCommandUtilities
     {
-        public static Embed GetModuleHelpEmbed(ModuleInfo module, ICommandContext context, IServiceProvider services)
-        {
-            var title = $"Help: **({module.Name + (module.Aliases.Count > 1 ? ($"/{string.Join("/", module.Aliases.Skip(1))}") : "")})**";
-            var validForCurrentUserCommands = module.Commands.Where(x => x.CheckPreconditionsAsync(context, services).GetAwaiter().GetResult().IsSuccess);
+        public static Embed GetModuleHelpEmbed(ModuleInfo module, ICommandContext context, IServiceProvider services, Config config) =>
+            new EmbedBuilder()
+                .BuildTitle(module)
+                .AddValidCommandFields(module, context, services, config)
+                .WithColor(Color.Blue)
+                .Build();
 
-            var embedBuilder = new EmbedBuilder().WithTitle(title).WithColor(Color.Blue);
-            foreach (var command in validForCurrentUserCommands)
+        private static EmbedBuilder AddValidCommandFields(this EmbedBuilder embedBuilder, ModuleInfo module, ICommandContext context, IServiceProvider services, Config config) 
+            => GetValidCommands(module, context, services)
+                .Aggregate(embedBuilder, (currentEmbedBuilder, nextCommand) 
+                    => currentEmbedBuilder.AddCommandField(nextCommand, module, config));
+
+        private static EmbedBuilder AddCommandField(this EmbedBuilder embedBuilder, CommandInfo command, ModuleInfo module, Config config)
+        {
+            var commandString = config.CommandPrefix;
+            if (!string.IsNullOrEmpty(module.Group))
             {
-                embedBuilder.AddField($"**{'!' + (string.IsNullOrEmpty(module.Group) ? "" : module.Group + " ") + command.Name}** " + GetParametersString(command).TrimEnd(' ', ','), $"{(command.Summary == string.Empty ? "No description" : command.Summary)}. ");
+                // Add the command group prefix
+                commandString += module.Group + " ";
             }
 
-            return embedBuilder.Build();
+            commandString += command.Name;
+
+            return embedBuilder.AddField($"**{commandString}** " + GetParametersString(command), GetCommandSummary(command));
+        }
+        private static IEnumerable<CommandInfo> GetValidCommands(ModuleInfo module, ICommandContext context, IServiceProvider services) 
+            => module.Commands.Where(x => x.CheckPreconditionsAsync(context, services).GetAwaiter().GetResult().IsSuccess);
+
+        private static EmbedBuilder BuildTitle(this EmbedBuilder embedBuilder, ModuleInfo module)
+        {
+            var moduleNamesString = module.Name;
+            if (module.Aliases.Count > 1)
+            {
+                // Skip 1 alias, as the first 'alias' is the full name
+                moduleNamesString += ($"/{string.Join("/", module.Aliases.Skip(1))}");
+            }
+
+            var title = $"Help: **({moduleNamesString})**";
+
+            return embedBuilder.WithTitle(title);
         }
 
         private static string GetSummaryString(string summary) => string.IsNullOrEmpty(summary) ? "" : $"({summary})";
-        private static string GetParametersString(CommandInfo command)
+
+        private static string GetParametersString(CommandInfo command) 
+            => command.Parameters.Aggregate(string.Empty, (current, parameter) 
+                => current + $"[{parameter.Name}{GetSummaryString(parameter.Summary)}] ");
+
+        private static string GetCommandSummary(CommandInfo command)
         {
-            var output = $"{command.Parameters.Aggregate("", (currentString, nextParameter) => currentString + $"[{nextParameter.Name}{(GetSummaryString(nextParameter.Summary) == string.Empty ? "" : GetSummaryString(nextParameter.Summary))}] ")}";
-            return output.Trim() == "Parameters:" ? "" : output;
+            if (string.IsNullOrEmpty(command.Summary)) return "No description provided";
+
+            if (command.Summary.EndsWith('.'))
+            {
+                return command.Summary;
+            }
+
+            return command.Summary + '.';
         }
     }
 }
