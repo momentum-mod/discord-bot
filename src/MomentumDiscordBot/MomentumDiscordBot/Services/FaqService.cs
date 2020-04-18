b5b452204a6a3f7281012f200b9427685aa12a15
Luke Parker <lukemparker@outlook.com>
2020-04-18 18:25:30 +1000

    Create FAQ service

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/FaqService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using MomentumDiscordBot.Models;
+using MomentumDiscordBot.Utilities;
+
+namespace MomentumDiscordBot.Services
+{
+    public class FaqService
+    {
+        private readonly Config _config;
+        private readonly DiscordSocketClient _discordClient;
+        private SocketTextChannel _textChannel;
+        private IMessage _lastMessage;
+
+        public FaqService(DiscordSocketClient discordClient, Config config)
+        {
+            _config = config;
+
+            _discordClient = discordClient;
+            _discordClient.Ready += _discordClient_Ready;
+        }
+
+        public async Task HookToLastMessageAsync()
+        {
+            _textChannel = _discordClient.GetChannel(_config.FaqChannelId) as SocketTextChannel;
+
+            if (_textChannel != null)
+            {
+                var messages = (await _textChannel.GetMessagesAsync().FlattenAsync()).ToList();
+
+                _lastMessage = messages.OrderByDescending(x => x.Timestamp.Ticks).FirstOrDefault();
+
+                // Remove all existing reactions
+                foreach (var message in messages)
+                {
+                    if (!(message is IUserMessage userMessage)) return;
+
+                    await userMessage.RemoveAllReactionsAsync();
+                }
+
+                if (_lastMessage is IUserMessage lastUserMessage)
+                {
+                    await lastUserMessage.AddReactionAsync(_config.MentionRoleEmoji);
+                }
+            }
+        }
+
+        private async Task _discordClient_Ready()
+        {
+            await HookToLastMessageAsync();
+            await VerifyCurrentUserRolesAsync();
+
+            _discordClient.ReactionAdded += ReactionAdded;
+        }
+        private async Task ReactionAdded(Cacheable<IUserMessage, ulong> messageBefore,
+            ISocketMessageChannel messageAfter, SocketReaction reaction)
+        {
+            if (reaction.Channel.Id != _textChannel.Id || !reaction.Emote.Equals(_config.MentionRoleEmoji)) return;
+
+            // Check that the message reacted to is the last message in the channel
+            if (_lastMessage.Id == reaction.MessageId)
+            {
+                // Get the user as a SocketGuildContext
+                var user = _discordClient.Guilds.First(x => x.Channels.Select(x => x.Id).Contains(messageAfter.Id))
+                    .Users.First(x => x.Id == reaction.UserId);
+
+                // Ignore actions from the bot
+                if (user.IsSelf(_discordClient)) return;
+
+                var role = _textChannel.Guild.GetRole(_config.FaqRoleId);
+                await user.AddRoleAsync(role);
+
+                if (_lastMessage is IUserMessage userMessage)
+                {
+                    await userMessage.RemoveReactionAsync(_config.MentionRoleEmoji, user);
+                }
+            }
+        }
+
+        private async Task VerifyCurrentUserRolesAsync()
+        {
+            var usersWithMentionRoles = _textChannel.Guild.Users.Where(x => _config.MentionRoles.Intersect(x.Roles.Select(y => y.Id)).Any()).ToList();
+
+            // Check users who have reacted to last message
+            var role = _textChannel.Guild.GetRole(_config.FaqRoleId);
+
+            if (!(_lastMessage is IUserMessage userMessage)) return;
+
+            // Get all users who have reacted to the embed
+            var reactionUsers = (await userMessage.GetReactionUsersAsync(_config.MentionRoleEmoji, _textChannel.Guild.MemberCount).FlattenAsync()).ToList();
+
+            foreach (var guildUser in reactionUsers.Where(user => !user.IsSelf(_discordClient))
+                .Where(user =>
+                    !usersWithMentionRoles.Any(x => x.Roles.Any(y => y.Id == _config.FaqRoleId) && x.Id == user.Id))
+                .Select(user => _textChannel.Guild.GetUser(user.Id)))
+            {
+                // Make sure the user is not null, in case they have been banned/left the server
+                if (guildUser != null)
+                {
+                    await guildUser.AddRoleAsync(role);
+                }
+            }
+
+            var userWithRole = usersWithMentionRoles.Where(x => x.Roles.Any(x => x.Id == _config.FaqRoleId));
+            foreach (var user in userWithRole)
+            {
+                if (reactionUsers.Any(x => x.Id == user.Id) && !user.IsSelf(_discordClient)) continue;
+
+                // User has not reacted, remove the role
+                var guildUser = _textChannel.Guild.GetUser(user.Id);
+                await guildUser.RemoveRoleAsync(role);
+            }
+        }
+    }
+}
