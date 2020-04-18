4ee3be78146f4832a213c22bfa8c184e50201acd
Luke Parker <lukemparker@outlook.com>
2020-04-18 18:25:58 +1000

    Allow FAQ service to be reloaded by an admin

--- src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/AdminModule.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/AdminModule.cs
@@ -11,6 +11,7 @@ namespace MomentumDiscordBot.Discord.Commands
     public class AdminModule : MomentumModuleBase
     {
         public StreamMonitorService StreamMonitorService { get; set; }
+        public FaqService FaqService { get; set; }
 
         [Command("updatestreams")]
         [Summary("Force an update of Twitch livestreams")]
@@ -19,6 +20,15 @@ namespace MomentumDiscordBot.Discord.Commands
             StreamMonitorService.UpdateCurrentStreamersAsync(null);
 
             await ReplyNewEmbedAsync("Updating Livestreams", Color.Blue);
+        }
+
+        [Command("reloadfaq")]
+        [Summary("Reloads the FAQ service, use when the messages change")]
+        public async Task ReloadFaqAsync()
+        {
+            await FaqService.HookToLastMessageAsync();
+
+            await ReplyNewEmbedAsync("Reloaded FAQ service", Color.Blue);
         }
     }
 }
