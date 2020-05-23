9eaf527b0785204ce8423e4a7477f528b1de0223
Luke Parker <lukemparker@outlook.com>
2020-05-23 17:06:27 +1000

    Move config properties to autoproperties without overriding set with SaveToFile

--- src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
@@ -10,235 +10,27 @@ namespace MomentumDiscordBot.Models
 {
     public class Config
     {
-        [JsonProperty("admin_id")] private ulong _adminRoleID;
-        [JsonProperty("command_prefix")] private string _commandPrefix;
-        [JsonProperty("livestream_mention_role_id")] private ulong _livestreamMentionRoleId;
-        [JsonProperty("mention_role_emoji")] private string _mentionRoleEmojiString;
-        [JsonProperty("mention_roles")] private ulong[] _mentionRoles;
-        [JsonProperty("moderator_id")] private ulong _moderatorRoleID;
-        [JsonProperty("streamer_channel")] private ulong _momentumModStreamerChannelId;
-        [JsonProperty("roles_channel")] private ulong _rolesChannelId;
-        [JsonProperty("twitch_user_bans")] private string[] _twitchUserBans;
-        [JsonProperty("admin_bot_channel")] private ulong _adminBotChannel;
-        [JsonProperty("stream_update_interval")] private int _streamUpdateInterval;
-        [JsonProperty("key_emoji")] private string _keyEmojiString;
-        [JsonProperty("key_begging_response")] private string _keyBeggingResponse;
-        [JsonProperty("key_regex")] private string _keyRegexString;
-        [JsonProperty("join_log_channel")] private ulong _joinLogChannel;
-        [JsonProperty("message_history_channel")] private ulong _messageHistoryChannel;
-        [JsonProperty("new_account_emote")] private string _newUserEmoteString;
-        [JsonProperty("whitelist_key_begging_roles")] private ulong[] _whitelistKeyBeggingRoles;
-        [JsonProperty("minimum_stream_viewers_announce")] private int _minimumStreamViewersAnnounce;
-
-        [JsonIgnore]
-        public ulong MomentumModStreamerChannelId
-        {
-            get => _momentumModStreamerChannelId;
-            set
-            {
-                _momentumModStreamerChannelId = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong RolesChannelId
-        {
-            get => _rolesChannelId;
-            set
-            {
-                _rolesChannelId = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public string CommandPrefix
-        {
-            get => _commandPrefix;
-            set
-            {
-                _commandPrefix = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong[] MentionRoles
-        {
-            get => _mentionRoles;
-            set
-            {
-                _mentionRoles = value.Distinct().ToArray();
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong[] WhitelistKeyBeggingRoles
-        {
-            get => _whitelistKeyBeggingRoles;
-            set
-            {
-                _whitelistKeyBeggingRoles = value.Distinct().ToArray();
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong ModeratorRoleID
-        {
-            get => _moderatorRoleID;
-            set
-            {
-                _moderatorRoleID = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong AdminRoleID
-        {
-            get => _adminRoleID;
-            set
-            {
-                _adminRoleID = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public string MentionRoleEmojiString
-        {
-            get => _mentionRoleEmojiString;
-            set
-            {
-                _mentionRoleEmojiString = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong LivestreamMentionRoleId
-        {
-            get => _livestreamMentionRoleId;
-            set
-            {
-                _livestreamMentionRoleId = value;
-                SaveToFile();
-            }
-        }
+        [JsonProperty("admin_id")] public ulong AdminRoleID { get; set; }
+        [JsonProperty("command_prefix")] public string CommandPrefix { get; set; }
+        [JsonProperty("livestream_mention_role_id")] public ulong LivestreamMentionRoleId { get; set; }
+        [JsonProperty("mention_role_emoji")] public string MentionRoleEmojiString { get; set; }
+        [JsonProperty("mention_roles")] public ulong[] MentionRoles { get; set; }
+        [JsonProperty("moderator_id")] public ulong ModeratorRoleID { get; set; }
+        [JsonProperty("streamer_channel")] public ulong MomentumModStreamerChannelId { get; set; }
+        [JsonProperty("roles_channel")] public ulong RolesChannelId { get; set; }
+        [JsonProperty("twitch_user_bans")] public string[] TwitchUserBans { get; set; }
+        [JsonProperty("admin_bot_channel")] public ulong AdminBotChannel { get; set; }
+        [JsonProperty("stream_update_interval")] public int StreamUpdateInterval { get; set; }
+        [JsonProperty("key_emoji")] public string KeyEmojiString { get; set; }
+        [JsonProperty("key_begging_response")] public string KeyBeggingResponse { get; set; }
+        [JsonProperty("key_regex")] public string KeyRegexString { get; set; }
+        [JsonProperty("join_log_channel")] public ulong JoinLogChannel { get; set; }
+        [JsonProperty("message_history_channel")] public ulong MessageHistoryChannel { get; set; }
+        [JsonProperty("new_account_emote")] public string NewUserEmoteString { get; set; }
+        [JsonProperty("whitelist_key_begging_roles")] public ulong[] WhitelistKeyBeggingRoles { get; set; }
+        [JsonProperty("minimum_stream_viewers_announce")] public int MinimumStreamViewersAnnounce { get; set; }
 
         [JsonIgnore] public Emoji MentionRoleEmoji => new Emoji(MentionRoleEmojiString);
-
-        [JsonIgnore]
-        public string[] TwitchUserBans
-        {
-            get => _twitchUserBans;
-            set
-            {
-                _twitchUserBans = value.Distinct().ToArray();
-                SaveToFile();
-            }
-        }
-        [JsonIgnore] 
-        public ulong AdminBotChannel
-        {
-            get => _adminBotChannel;
-            set
-            {
-                _adminBotChannel = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public int StreamUpdateInterval
-        {
-            get => _streamUpdateInterval;
-            set
-            {
-                _streamUpdateInterval = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public string KeyEmojiString
-        {
-            get => _keyEmojiString;
-            set
-            {
-                _keyEmojiString = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public string KeyRegexString
-        {
-            get => _keyRegexString;
-            set
-            {
-                _keyRegexString = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong JoinLogChannel
-        {
-            get => _joinLogChannel;
-            set
-            {
-                _joinLogChannel = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public string KeyBeggingResponse
-        {
-            get => _keyBeggingResponse;
-            set
-            {
-                _keyBeggingResponse = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public ulong MessageHistoryChannel
-        {
-            get => _messageHistoryChannel;
-            set
-            {
-                _messageHistoryChannel = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public string NewUserEmoteString
-        {
-            get => _newUserEmoteString;
-            set
-            {
-                _newUserEmoteString = value;
-                SaveToFile();
-            }
-        }
-
-        [JsonIgnore]
-        public int MinimumStreamViewersAnnounce
-        {
-            get => _minimumStreamViewersAnnounce;
-            set
-            {
-                _minimumStreamViewersAnnounce = value;
-                SaveToFile();
-            }
-        }
         public static Config LoadFromFile()
         {
             if (File.Exists(PathConstants.ConfigFilePath))
