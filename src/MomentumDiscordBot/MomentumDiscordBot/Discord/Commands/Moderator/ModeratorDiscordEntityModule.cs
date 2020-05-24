ad11c0fb37988134689c5ff41c062fbc4c757700
Luke Parker <lukemparker@outlook.com>
2020-05-24 19:02:36 +1000

    Create ModeratorDiscordEntityModule.cs

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/Moderator/ModeratorDiscordEntityModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using MomentumDiscordBot.Utilities;
+
+namespace MomentumDiscordBot.Discord.Commands.Moderator
+{
+    [Group("discord")]
+    public class ModeratorDiscordEntityModule : ModeratorModule
+    {
+    }
+}
