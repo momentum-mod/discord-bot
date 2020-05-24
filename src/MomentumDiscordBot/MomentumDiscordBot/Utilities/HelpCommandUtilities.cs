860af2f09d68672728981897b4e7269834eb0c20
Luke Parker <lukemparker@outlook.com>
2020-05-24 19:00:59 +1000

    Create a hidden command attribute

--- src/MomentumDiscordBot/MomentumDiscordBot/Utilities/HelpCommandUtilities.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Utilities/HelpCommandUtilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Discord;
 using Discord.Commands;
+using MomentumDiscordBot.Discord.Commands;
 using MomentumDiscordBot.Models;
 
 namespace MomentumDiscordBot.Utilities
@@ -35,7 +36,7 @@ namespace MomentumDiscordBot.Utilities
             return embedBuilder.AddField($"**{commandString}** " + GetParametersString(command), GetCommandSummary(command));
         }
         private static IEnumerable<CommandInfo> GetValidCommands(ModuleInfo module, ICommandContext context, IServiceProvider services) 
-            => module.Commands.Where(x => x.CheckPreconditionsAsync(context, services).GetAwaiter().GetResult().IsSuccess);
+            => module.Commands.Where(x => x.Attributes.All(y => y.GetType() != typeof(HiddenAttribute)) && x.CheckPreconditionsAsync(context, services).GetAwaiter().GetResult().IsSuccess);
 
         private static EmbedBuilder BuildTitle(this EmbedBuilder embedBuilder, ModuleInfo module)
         {
