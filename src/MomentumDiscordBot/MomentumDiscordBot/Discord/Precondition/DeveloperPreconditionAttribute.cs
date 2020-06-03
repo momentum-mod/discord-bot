02b20212d020dd5aafb76e705a2867d3a3591261
Luke Parker <lukemparker@outlook.com>
2020-06-03 15:55:15 +1000

    Create DeveloperPreconditionAttribute.cs

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Precondition/DeveloperPreconditionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using MomentumDiscordBot.Models;
+using MomentumDiscordBot.Utilities;
+
+namespace MomentumDiscordBot.Discord.Precondition
+{
+    public class DeveloperPreconditionAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command,
+            IServiceProvider services)
+        {
+            var config = services.GetRequiredService<Config>();
+
+            if (context.User.Id == config.DeveloperID)
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+
+            return Task.FromResult(PreconditionResult.FromError("You are not the developer"));
+        }
+    }
+}
