0c9bf836ecfb55914fb0b552a9dbfe5ad0377bae
Luke Parker <lukemparker@outlook.com>
2020-06-10 23:03:26 +1000

    Patch zero day bug removing everyone from the FAQ role
    Yikes...

--- src/MomentumDiscordBot/MomentumDiscordBot/Services/FaqService.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Services/FaqService.cs
@@ -83,7 +83,6 @@ namespace MomentumDiscordBot.Services
             _ = Task.Run(async () =>
             {
                 await HookToLastMessageAsync();
-                await VerifyCurrentUserRolesAsync();
             });
 
             return Task.CompletedTask;
@@ -110,41 +109,6 @@ namespace MomentumDiscordBot.Services
                 {
                     await userMessage.RemoveReactionAsync(_config.MentionRoleEmoji, user);
                 }
-            }
-        }
-
-        private async Task VerifyCurrentUserRolesAsync()
-        {
-            var usersWithMentionRoles = _textChannel.Guild.Users.Where(x => _config.MentionRoles.Intersect(x.Roles.Select(y => y.Id)).Any()).ToList();
-
-            // Check users who have reacted to last message
-            var role = _textChannel.Guild.GetRole(_config.FaqRoleId);
-
-            if (!(_lastMessage is IUserMessage userMessage)) return;
-
-            // Get all users who have reacted to the embed
-            var reactionUsers = (await userMessage.GetReactionUsersAsync(_config.MentionRoleEmoji, _textChannel.Guild.MemberCount).FlattenAsync()).ToList();
-
-            foreach (var guildUser in reactionUsers.Where(user => !user.IsSelf(_discordClient))
-                .Where(user =>
-                    !usersWithMentionRoles.Any(x => x.Roles.Any(y => y.Id == _config.FaqRoleId) && x.Id == user.Id))
-                .Select(user => _textChannel.Guild.GetUser(user.Id)))
-            {
-                // Make sure the user is not null, in case they have been banned/left the server
-                if (guildUser != null)
-                {
-                    await guildUser.AddRoleAsync(role);
-                }
-            }
-
-            var userWithRole = usersWithMentionRoles.Where(x => x.Roles.Any(x => x.Id == _config.FaqRoleId));
-            foreach (var user in userWithRole)
-            {
-                if (reactionUsers.Any(x => x.Id == user.Id) && !user.IsSelf(_discordClient)) continue;
-
-                // User has not reacted, remove the role
-                var guildUser = _textChannel.Guild.GetUser(user.Id);
-                await guildUser.RemoveRoleAsync(role);
             }
         }
     }
