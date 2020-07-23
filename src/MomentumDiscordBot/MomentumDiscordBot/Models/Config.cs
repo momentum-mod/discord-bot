d97c7eb4235fb09cb99da7308f9c2bf584da6e83
Luke Parker <lukemparker@outlook.com>
2020-07-23 18:32:25 +1000

    Hide the connection string from config get

--- src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Config.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using Discord;
 using MomentumDiscordBot.Constants;
+using MomentumDiscordBot.Discord.Commands;
 using Newtonsoft.Json;
 
 namespace MomentumDiscordBot.Models
@@ -36,6 +37,7 @@ namespace MomentumDiscordBot.Models
         [JsonProperty("faq_role")] public ulong FaqRoleId { get; set; }
         [JsonProperty("developer_id")] public ulong DeveloperID { get; set; }
         [JsonProperty("alt_account_emoji")] public string AltAccountEmojiString { get; set; }
+        [Hidden]
         [JsonProperty("mysql_connection_string")] public string MySqlConnectionString { get; set; }
 
         [JsonIgnore] public Emoji MentionRoleEmoji => new Emoji(MentionRoleEmojiString);
