e99dcaa88118eb9a020281dcb49a72e5c51a7ee3
Luke Parker <lukemparker@outlook.com>
2020-07-23 18:28:29 +1000

    Create MomentumDiscordDbContext.cs

---
+++ src/MomentumDiscordBot/MomentumDiscordBot/Models/Data/MomentumDiscordDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MomentumDiscordBot.Models.Data
+{
+    public class MomentumDiscordDbContext : DbContext
+    {
+        public MomentumDiscordDbContext(DbContextOptions<MomentumDiscordDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DailyMessageCount>()
+                .HasKey(x => new {x.UserId, x.ChannelId, x.Date});
+        }
+
+        public DbSet<DailyMessageCount> DailyMessageCount { get; set; }
+    }
+}
