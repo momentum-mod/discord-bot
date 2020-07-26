06203902222b8e092d0bcf6b8d5f4f0a991b13ad
Luke Parker <lukemparker@outlook.com>
2020-07-26 10:19:04 +1000

    Auto clean code

--- src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/StatsTopModule.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Discord/Commands/StatsTopModule.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using MomentumDiscordBot.Constants;
 using MomentumDiscordBot.Models;
 using MomentumDiscordBot.Utilities;
 
@@ -32,7 +28,7 @@ namespace MomentumDiscordBot.Discord.Commands
         {
             var topChannels = await StatsUtility.GetTopMessages(Config, x => x.ChannelId);
 
-            var embedBuilder = topChannels.GetTopStatsEmbedBuilder("Most Active Channels", 
+            var embedBuilder = topChannels.GetTopStatsEmbedBuilder("Most Active Channels",
                 x => $"{MentionUtils.MentionChannel(x.Grouping)} - {x.MessageCount} messages");
 
             await ReplyAsync(embed: embedBuilder.Build());
--- src/MomentumDiscordBot/MomentumDiscordBot/Utilities/StatsUtility.cs
+++ src/MomentumDiscordBot/MomentumDiscordBot/Utilities/StatsUtility.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using MomentumDiscordBot.Constants;
@@ -13,18 +11,20 @@ namespace MomentumDiscordBot.Utilities
 {
     public static class StatsUtility
     {
-        public static async Task<List<(T Grouping, long MessageCount)>> GetTopMessages<T>(Config config, Func<DailyMessageCount, T> groupFunc)
+        public static async Task<List<(T Grouping, long MessageCount)>> GetTopMessages<T>(Config config,
+            Func<DailyMessageCount, T> groupFunc)
         {
             await using var dbContext = DbContextHelper.GetNewDbContext(config);
 
             return dbContext.DailyMessageCount.ToList().GroupBy(groupFunc)
-                .Select(x => (Grouping:x.Key, MessageCount:x.ToList().Sum(x => x.MessageCount)))
+                .Select(x => (Grouping: x.Key, MessageCount: x.ToList().Sum(x => x.MessageCount)))
                 .OrderByDescending(x => x.MessageCount)
                 .Take(10)
                 .ToList();
         }
 
-        public static EmbedBuilder GetTopStatsEmbedBuilder<T>(this List<(T Grouping, long MessageCount)> topStats, string title,
+        public static EmbedBuilder GetTopStatsEmbedBuilder<T>(this List<(T Grouping, long MessageCount)> topStats,
+            string title,
             Func<(T Grouping, long MessageCount), string> elementStringConverterFunc) =>
             new EmbedBuilder
             {
