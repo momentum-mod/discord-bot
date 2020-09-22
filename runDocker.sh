7e30a2e26866213c75271f041c9c0354fc3e83a4
TheRealHona <lukemparker@outlook.com>
2020-09-22 02:09:23 +0000

    Always restart bot, and fix order of docker commands

--- runDocker.sh
+++ runDocker.sh
@@ -1,8 +1,11 @@
 echo -e '-= Stopping MMOD Discord Bot Container =-\n'
 docker container stop momentum-discord-production
 
+echo -e '-= Removing MMOD Discord Bot Container =-\n'
+docker container rm momentum-discord-production
+
 echo -e '-= Pulling MMOD Discord Bot Image from GitHub Packages =-\n'
 docker pull docker.pkg.github.com/momentum-mod/discord-bot/mmod-discord-bot:net-core
 
 echo -e '-= Runnning the MMOD Discord Bot Image =-\n'
-docker run -v $PWD/config:/app/config --network host --restart on-failure:5 --name "momentum-discord-production" -d docker.pkg.github.com/momentum-mod/discord-bot/mmod-discord-bot:net-core
+docker run -v $PWD/config:/app/config --network host --restart always --name "momentum-discord-production" -d docker.pkg.github.com/momentum-mod/discord-bot/mmod-discord-bot:net-core
