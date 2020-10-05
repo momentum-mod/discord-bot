93187aa0f30c487f39f99eea811682ca504807d5
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:36:30 +1000

    Create DailyMessageCount.cs

---
+++ MomentumDiscordBot/Models/Data/DailyMessageCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MomentumDiscordBot.Models.Data
+{
+    [Table("message_count")]
+    public class DailyMessageCount
+    {
+        [Required]
+        [Column(TypeName = "BIGINT UNSIGNED NOT NULL")]
+        public ulong UserId { get; set; }
+
+        [Required]
+        [Column(TypeName = "BIGINT UNSIGNED NOT NULL")]
+        public ulong ChannelId { get; set; }
+
+        [Required]
+        [Column(TypeName = "DATE NOT NULL")]
+        public DateTime Date { get; set; }
+
+        [Column(TypeName = "MEDIUMINT UNSIGNED")]
+        public uint MessageCount { get; set; }
+    }
+}
