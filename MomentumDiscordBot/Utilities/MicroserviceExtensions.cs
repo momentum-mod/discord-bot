bec2fe5f04ee927a472fd5c45455a025a9d073b6
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:33:55 +1000

    Create method to automatically inject microservices

--- MomentumDiscordBot/Utilities/MicroserviceExtensions.cs
+++ MomentumDiscordBot/Utilities/MicroserviceExtensions.cs
@@ -8,4 +8,28 @@ namespace MomentumDiscordBot.Utilities
 {
     public static class MicroserviceExtensions
     {
+        public static IServiceCollection InjectMicroservices(this IServiceCollection services, Assembly assembly)
+        {
+            var types = assembly.ExportedTypes.Where(type =>
+            {
+                var typeInfo = type.GetTypeInfo();
+
+                // Does it have the `MicroserviceAttribute` 
+                return typeInfo.GetCustomAttributes().Any(x => x.GetType() == typeof(MicroserviceAttribute));
+            }).ToList();
+
+            foreach (var type in types)
+            {
+                var microserviceAttribute = type.GetCustomAttribute<MicroserviceAttribute>();
+
+                if (microserviceAttribute != null && (microserviceAttribute.Type == MicroserviceType.Inject ||
+                                                      microserviceAttribute.Type ==
+                                                      MicroserviceType.InjectAndInitialize))
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
 }
