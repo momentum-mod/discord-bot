b9f22be10a994d8c6ccf89f23604232abe8327e3
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:31:35 +1000

    Create async program.cs

---
+++ MomentumDiscordBot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using MomentumDiscordBot.Models;
+using Serilog;
+using Serilog.Events;
+
+namespace MomentumDiscordBot
+{
+    public static class Program
+    {
+        internal static void Main()
+            => MainAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+        }
+    }
+}
