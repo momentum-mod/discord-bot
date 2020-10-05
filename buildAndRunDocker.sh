21dddd09cd44fc52c6b50449dc759543f1daff9d
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:29:04 +1000

    Update buildAndRunDocker.sh

--- buildAndRunDocker.sh
+++ buildAndRunDocker.sh
@@ -5,7 +5,7 @@ echo -e '-= Removing Old momentum-discord Production Container =-\n'
 docker container rm momentum-discord-production
 
 echo -e '-= Building Docker Image from Dockerfile =-\n'
-docker build -t momentum-discord -f ./src/MomentumDiscordBot/MomentumDiscordBot/Dockerfile .
+docker build -t momentum-discord -f ./MomentumDiscordBot/Dockerfile ./MomentumDiscordBot
 
 echo -e '-= Running the Image =-\n'
 docker run -v $PWD/config:/app/config --network host --restart on-failure:5 --name "momentum-discord-production" -d momentum-discord
