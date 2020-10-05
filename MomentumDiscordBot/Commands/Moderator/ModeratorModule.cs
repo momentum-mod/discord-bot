012cb701796c8b7fe93314c1315f9006fb365af2
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:36:47 +1000

    Create ModeratorModule.cs

---
+++ MomentumDiscordBot/Commands/Moderator/ModeratorModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using MomentumDiscordBot.Constants;
+using MomentumDiscordBot.Models;
+
+namespace MomentumDiscordBot.Commands.Moderator
+{
+    public class ModeratorModule : ModeratorModuleBase
+    {
+        public Configuration Config { get; set; }
+
+
+        [Command("ban")]
+        [Description("Bans a user, purging their messages")]
+        public async Task BanAsync(CommandContext context, DiscordMember member)
+        {
+            await member.BanAsync(7, $"Banned by {context.User} using !ban");
+            await ReplyNewEmbedAsync(context, $"Banned {member}, purging their messages in the last 7 days.",
+                MomentumColor.Red);
+        }
+    }
+}
