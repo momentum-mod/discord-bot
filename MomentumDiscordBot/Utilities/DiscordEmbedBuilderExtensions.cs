5983f43636c07cd1d4a1f9c0353d7563e1519212
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:34:57 +1000

    Port DiscordEmbedBuilderExtensions.cs

---
+++ MomentumDiscordBot/Utilities/DiscordEmbedBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using DSharpPlus.Entities;
+
+namespace MomentumDiscordBot.Utilities
+{
+    public static class DiscordEmbedBuilderExtensions
+    {
+        public static DiscordEmbedBuilder AddMessageContent(this DiscordEmbedBuilder embedBuilder,
+            DiscordMessage message)
+        {
+            if (message.Author != null)
+            {
+                embedBuilder.AddField("User",
+                    $"{message.Author.Mention} {message.Author}");
+            }
+
+            if (message.Channel != null)
+            {
+                embedBuilder.AddField("Channel", message.Channel.Mention);
+            }
+
+            if (!string.IsNullOrWhiteSpace(message.Content))
+            {
+                embedBuilder.AddField("Message", string.Join(string.Empty, message.Content.Take(1024)));
+
+                if (message.Content.Length > 1024)
+                {
+                    embedBuilder.AddField("Message Overflow",
+                        string.Join(string.Empty, message.Content.Skip(1024)));
+                }
+            }
+
+            var attachments = message.Attachments.ToList();
+            for (var i = 0; i < attachments.Count; i++)
+            {
+                var attachment = attachments[i];
+
+                embedBuilder.AddField($"Attachment {i + 1}", attachment.Url);
+            }
+
+            return embedBuilder;
+        }
+    }
+}
