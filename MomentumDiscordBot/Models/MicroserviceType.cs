64d29899dfc1b4207790c66f6b0e0a5f9ffd6f12
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:34:32 +1000

    Add microservice enum + attribute

---
+++ MomentumDiscordBot/Models/MicroserviceType.cs
@@ -0,0 +1,20 @@
+﻿namespace MomentumDiscordBot.Models
+{
+    public enum MicroserviceType
+    {
+        /// <summary>
+        ///     Does nothing with the service automatically
+        /// </summary>
+        Manual = 0,
+
+        /// <summary>
+        ///     Adds the service to the DI provider
+        /// </summary>
+        Inject = 1,
+
+        /// <summary>
+        ///     Initializes the services through `IServiceProvider#GetRequiredService()`
+        /// </summary>
+        InjectAndInitialize = 1 << 1
+    }
+}
