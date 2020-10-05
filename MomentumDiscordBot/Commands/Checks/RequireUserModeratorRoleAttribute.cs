0453aa4fd03e38ecbefcb063306b6433f7e8498a
Luke Parker <lukemparker@outlook.com>
2020-10-05 17:36:57 +1000

    Create RequireUserModeratorRoleAttribute.cs

---
+++ MomentumDiscordBot/Commands/Checks/RequireUserModeratorRoleAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace MomentumDiscordBot.Commands.Checks
+{
+    public class RequireUserModeratorRoleAttribute : RequireUserRoleAttribute
+    {
+        public RequireUserModeratorRoleAttribute()
+        {
+            RoleIdSelector = configuration => configuration.ModeratorRoleID;
+            FailureResponse = "Missing the Moderator role";
+        }
+    }
+}
