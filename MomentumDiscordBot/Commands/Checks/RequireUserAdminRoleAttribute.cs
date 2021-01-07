18b6aefad5a8001bac0dd40350e8b0318dc7ae08
Luke Parker <lukemparker@outlook.com>
2021-01-07 18:20:28 +1000

    Refactor dev override to use config

--- MomentumDiscordBot/Commands/Checks/RequireUserAdminRoleAttribute.cs
+++ MomentumDiscordBot/Commands/Checks/RequireUserAdminRoleAttribute.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.DependencyInjection;
+using MomentumDiscordBot.Models;
 
 namespace MomentumDiscordBot.Commands.Checks
 {
@@ -8,8 +10,14 @@ namespace MomentumDiscordBot.Commands.Checks
         public override async Task<bool> ExecuteCheckAsync(CommandContext context, bool help)
         {
             var baseExecutionResult = await base.ExecuteCheckAsync(context, help);
+            if (baseExecutionResult)
+            {
+                return true;
+            }
 
-            return baseExecutionResult || context.User.Id == 115349553770659841;
+            var config = context.CommandsNext.Services.GetRequiredService<Configuration>();
+            
+            return context.User.Id == config.DeveloperID;
         }
 
         public RequireUserAdminRoleAttribute()
