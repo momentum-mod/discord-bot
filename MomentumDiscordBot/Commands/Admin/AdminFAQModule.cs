7b1e003ea57a45b5d99cb1ca5cbd04e77690271e
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-01-25 10:30:36 +1000

    Update !faq lock

--- MomentumDiscordBot/Commands/Admin/AdminFAQModule.cs
+++ MomentumDiscordBot/Commands/Admin/AdminFAQModule.cs
@@ -3,62 +3,20 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using MomentumDiscordBot.Constants;
 using MomentumDiscordBot.Services;
+using MomentumDiscordBot.Commands.Moderator;
 
 namespace MomentumDiscordBot.Commands.Admin
 {
     [Group("faq")]
-    public class AdminFAQModule : AdminModuleBase
+    public class AdminFAQModule : ModeratorModuleBase
     {
-        public FaqService FaqService { get; set; }
-
-        [Command("reload")]
-        [Description("Reloads the FAQ service, use when the messages change")]
-        public async Task ReloadFaqAsync(CommandContext context)
-        {
-            await ReplyNewEmbedAsync(context, "Reloading FAQ service...", MomentumColor.Blue);
-            await FaqService.HookToLastMessageAsync();
-
-            await ReplyNewEmbedAsync(context, "Reloaded FAQ service", MomentumColor.Blue);
-        }
 
         [Command("lock")]
         [Description("Locks users from being verified from the FAQ channel, use when getting spammed by user bots")]
         public async Task LockFaqAsync(CommandContext context)
         {
-            FaqService.Lock();
-
-            await ReplyNewEmbedAsync(context, "Locked users from being verified in the FAQ channel",
+            await ReplyNewEmbedAsync(context, "Since carlbot handles the FAQ roles now, in the case of a raid, please edit the `@everyone` permission override to deny viewing the channel - this will disable new user from being verified.",
                 MomentumColor.Blue);
-        }
-
-        [Command("unlock")]
-        [Description("Allows users to be verified from the FAQ channel")]
-        public async Task UnlockFaqAsync(CommandContext context)
-        {
-            await FaqService.UnlockAsync();
-
-            await ReplyNewEmbedAsync(context, "Allowed users to be verified in the FAQ channel", MomentumColor.Blue);
-        }
-
-        [Command("verify")]
-        [Description("Checks if there are any unhandled reactions")]
-        public async Task VerifyFaqAsync(CommandContext context)
-        {
-            await FaqService.AddUnhandedReactionRolesAsync();
-
-            await ReplyNewEmbedAsync(context, "Done", MomentumColor.Blue);
-        }
-
-        [Command("fix")]
-        [Description(
-            "Removes all reactions from the FAQ message, then reloads the module. A quick fix to the hidden reaction bug")]
-        public async Task FixFaqReactionAsync(CommandContext context)
-        {
-            await ReplyNewEmbedAsync(context, "Clearing all reactions", MomentumColor.Blue);
-            
-            await FaqService.FaqMessage.DeleteAllReactionsAsync();
-
-            await ReloadFaqAsync(context);
         }
     }
 }
