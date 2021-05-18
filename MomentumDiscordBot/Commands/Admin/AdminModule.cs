038421ad084fca70dbfffac3df5cb919444b0077
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-05-18 15:24:49 +1000

    Implement a force restart command

--- MomentumDiscordBot/Commands/Admin/AdminModule.cs
+++ MomentumDiscordBot/Commands/Admin/AdminModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -28,6 +29,18 @@ namespace MomentumDiscordBot.Commands.Admin
             await DiscordClient.DisconnectAsync();
             await Task.Delay(seconds * 1000);
             await DiscordClient.ReconnectAsync();
+        }
+        
+        [Command("forcerestart")]
+        [Description("Forces the bot to exit the process, and have Docker auto-restart it")]
+        public Task ForceRestartAsync(CommandContext context)
+        {
+            Logger.Warning("{User} forced the bot to restart", context.User);
+            
+            // Safe exit
+            Environment.Exit(0);
+
+            return Task.CompletedTask;
         }
     }
 }
