14aa9f572a047a7b3476f0ff6968d4e23589993f
BIRD <xbird@t-online.de>
2022-06-10 17:51:48 +0200

    fixed "add as cust..." grabbing video embeds

--- MomentumDiscordBot/Commands/General/GeneralModule.cs
+++ MomentumDiscordBot/Commands/General/GeneralModule.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.Entities;
 using MomentumDiscordBot.Models;
@@ -13,19 +15,39 @@ namespace MomentumDiscordBot.Commands.General
     {
         public Configuration Config { get; set; }
 
-        [SlashCommand("say", "executes a custom command")]
+        public const string SayCommandName = "say";
+        [SlashCommand(SayCommandName, "executes a custom command")]
         public async Task ExecCustomCommandAsync(InteractionContext context, [Autocomplete(typeof(AutoCompleteProvider))][Option("option", "name of the custom command")] string name)
         {
             CustomCommand command;
             if (Config.CustomCommands.TryGetValue(name, out command))
             {
-                var embed = new DiscordEmbedBuilder
+                if (string.IsNullOrWhiteSpace(command.Title) && string.IsNullOrWhiteSpace(command.Description))
+                {
+                    //discord refuses to send messages without content
+                    command.Title = "<title here!>";
+                }
+                var embedBuilder = new DiscordEmbedBuilder
                 {
                     Title = command.Title,
                     Description = command.Description,
                     Color = MomentumColor.Blue
-                }.Build();
-                await context.CreateResponseAsync(embed: embed);
+                };
+                if (Uri.IsWellFormedUriString(command.ThumbnailUrl, UriKind.Absolute))
+                {
+                    embedBuilder.Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
+                    {
+                        Url = command.ThumbnailUrl,
+                        Height = 90,
+                        Width = 160
+                    };
+                }
+                var message = new DiscordMessageBuilder()
+                                .AddEmbed(embedBuilder.Build());
+
+                if (Uri.IsWellFormedUriString(command.ButtonUrl, UriKind.Absolute))
+                    message.AddComponents(new DiscordLinkButtonComponent(command.ButtonUrl, command.ButtonLabel ?? "link"));
+                await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(message));
             }
             else
             {
