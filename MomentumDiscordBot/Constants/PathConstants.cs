3d313dfc014a771e788963901d4e095e416c6e44
tsa96 <tom@trumpetdust.org>
2022-10-08 09:08:35 +0100

    feat: Switch from MySQL to SQLite

--- MomentumDiscordBot/Constants/PathConstants.cs
+++ MomentumDiscordBot/Constants/PathConstants.cs
@@ -7,5 +7,8 @@ namespace MomentumDiscordBot.Constants
     {
         private static readonly string ConfigFolderPath = Path.Combine(Environment.CurrentDirectory, "config");
         internal static readonly string ConfigFilePath = Path.Combine(ConfigFolderPath, "config.json");
+        
+        private static readonly string DbFolderPath = Path.Combine(Environment.CurrentDirectory, "data");
+        internal static readonly string DbFilePath = Path.Combine(DbFolderPath, "bot_data.db");
     }
 }
